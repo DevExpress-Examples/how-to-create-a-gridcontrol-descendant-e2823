5547e8a5e71663505e59782119403e6542d45cbe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MyGridControl/CustomGridControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Core.WPFCompatibility;
+
+namespace CustomGridControl {
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomGridControl"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomGridControl;assembly=CustomGridControl"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+
+    public class CustomGridControl : GridControl {
+        protected override void OnPropertyChanged(SLDependencyPropertyChangedEventArgs e) {
+            if (e.Property == GridControl.FilterCriteriaProperty) {
+                MessageBox.Show(e.NewValue.ToString());
+            }
+            base.OnPropertyChanged(e);
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to create a GridControl descendant
+
+
+<p>You should inherit from the GridControl, override the necessary method, and embed this custom GridControl into your XAML. This sample illustrates how to override the OnPropertyChanged method to be notified that the FilterCriteria property has been changed.<br />
+</p>
+
+<br/>
+
+
